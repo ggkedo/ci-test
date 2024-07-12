55a271bc734cdecd34d61d87a942903b8d2a8c1d
ggkedo <34963190+ggkedo@users.noreply.github.com>
2024-07-12 15:40:16 +0200

    added routes template

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,14 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/requests', 'Home::index');
+
+$routes->get('/requests');
 $routes->get('/requests/(:num)', 'Requests::GetRequestById/$1');
+$routes->get('/requests/new', 'Home::index');
+
+$routes->get('/sample/(:num)', '');
+$routes->get('/sample/(:num)/details', '');
+$routes->get('/sample/new', '');
+
+$routes->get('/measurement/(:num)', '');
+$routes->get('/measurement/new', '');
