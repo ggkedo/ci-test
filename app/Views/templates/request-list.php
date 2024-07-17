e200bbe95e47319fa997c551b67f79a3d1b39814
ggkedo <34963190+ggkedo@users.noreply.github.com>
2024-07-17 16:06:54 +0200

    renamed file

--- app/Views/templates/request-list.php
+++ app/Views/templates/request-list.php
@@ -14,33 +14,33 @@
 
 <?php foreach($requests as $request):?>
 		<tr>
-			<td><?= $request->RequestedByEmail ?></td>
+			<td><?= $request->Status ?></td>
 			<td><?= $request->ProjectId ?></td>
 			<td><?= $request->Modified ?></td>
-			<td><a href="<?= base_url('request/edit/' . $request->id)?>" title="Szerkesztés"><i class="fa-solid fa-pen h5"></i></a></td>
+			<td><a href="<?= base_url('request/edit/' . $request->ID)?>" title="Szerkesztés"><i class="fa-solid fa-pen h5"></i></a></td>
 			<td>
-				<a href="" title="Eltávolítás" class="link-danger" data-bs-toggle="modal" data-bs-target="#delModal<?= $request->id ?>"><i class="fa-solid fa-trash h5"></i></a>
+				<a href="" title="Eltávolítás" class="link-danger" data-bs-toggle="modal" data-bs-target="#delModal<?= $request->ID ?>"><i class="fa-solid fa-trash h5"></i></a>
 
 				<!-- The Modal -->
-				<div class="modal fade" id="delModal<?= $project->id ?>">
+				<div class="modal fade" id="delModal<?= $request->ID ?>">
 				  <div class="modal-dialog">
 					<div class="modal-content">
 
 					  <!-- Modal Header -->
 					  <div class="modal-header">
-						<h4 class="modal-title">Törlés: <?= $request->name ?></h4>
+						<h4 class="modal-title">Törlés: <?= $request->ProjectId ?></h4>
 						<button type="button" class="btn-close" data-bs-dismiss="modal"></button>
 					  </div>
 
 					  <!-- Modal body -->
 					  <div class="modal-body">
-						  A következő eltávolításra fog kerülni: <?= $request->id ?>. <br>Jóváhagyást követően a projekthez tartozó összes adat véglegesen törlésre kerül (a hozzátartozó feladatok is).<br><br>Biztosan törli?
+						  A következő eltávolításra fog kerülni: <?= $request->ID ?>. <br>Jóváhagyást követően a projekthez tartozó összes adat véglegesen törlésre kerül (a hozzátartozó feladatok is).<br><br>Biztosan törli?
 					  </div>
 
 					  <!-- Modal footer -->
 					  <div class="modal-footer">
 						  <form method="post" action="<?= base_url('request') ?>">
-							<button name="del" value="<?= $project->id ?>" type="submit" class="btn btn-danger">Igen, törlöm</button>
+							<button name="del" value="<?= $request->ID ?>" type="submit" class="btn btn-danger">Igen, törlöm</button>
 							<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Mégsem</button>
 						  </form>
 					  </div>
