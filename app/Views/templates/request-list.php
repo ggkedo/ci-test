e6f8db015a74fe9dc8e734f47b23d19ba0737661
ggkedo <34963190+ggkedo@users.noreply.github.com>
2024-07-19 15:36:40 +0200

    translation

--- app/Views/templates/request-list.php
+++ app/Views/templates/request-list.php
@@ -17,9 +17,9 @@
 			<td><?= $request->RequestorEmail ?></td>
 			<td><?= $request->Modified ?></td>
 			<td><?= $request->ProjectId ?></td>
-			<td><a href="<?= base_url('request/edit/' . $request->ID)?>" title="Szerkesztés"><i class="fa-solid fa-list h5"></i></a></td>
+			<td><a href="<?= base_url('request/edit/' . $request->ID)?>" title="Edit"><i class="fa-solid fa-list h5"></i></a></td>
 			<td>
-				<a href="" title="Eltávolítás" class="link-danger" data-bs-toggle="modal" data-bs-target="#delModal<?= $request->ID ?>"><i class="fa-solid fa-trash h5"></i></a>
+				<a href="" title="Delete" class="link-danger" data-bs-toggle="modal" data-bs-target="#delModal<?= $request->ID ?>"><i class="fa-solid fa-trash h5"></i></a>
 
 				<!-- The Modal -->
 				<div class="modal fade" id="delModal<?= $request->ID ?>">
@@ -28,20 +28,20 @@
 
 					  <!-- Modal Header -->
 					  <div class="modal-header">
-						<h4 class="modal-title">Törlés: <?= $request->ProjectId ?></h4>
+						<h4 class="modal-title">Delete: <?= $request->ProjectId ?></h4>
 						<button type="button" class="btn-close" data-bs-dismiss="modal"></button>
 					  </div>
 
 					  <!-- Modal body -->
 					  <div class="modal-body">
-						  A következő eltávolításra fog kerülni: <?= $request->ID ?>. <br>Jóváhagyást követően a projekthez tartozó összes adat véglegesen törlésre kerül (a hozzátartozó feladatok is).<br><br>Biztosan törli?
+						  The following request will be deleted <?= $request->ID ?>. <br>This will also delete all samples and measurements associated with this request.<br><br>Are you sure?
 					  </div>
 
 					  <!-- Modal footer -->
 					  <div class="modal-footer">
 						  <form method="post" action="<?= base_url('request') ?>">
-							<button name="del" value="<?= $request->ID ?>" type="submit" class="btn btn-danger">Igen, törlöm</button>
-							<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Mégsem</button>
+							<button name="del" value="<?= $request->ID ?>" type="submit" class="btn btn-danger">Yes</button>
+							<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
 						  </form>
 					  </div>
 
