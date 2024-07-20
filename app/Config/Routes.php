feccb66b6d3ef530433ea8074b6ae0a97256c3dc
ggkedo <34963190+ggkedo@users.noreply.github.com>
2024-07-20 22:04:47 +0200

    redesigned request controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,17 +8,20 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 $routes->get('/requests', 'Requests::List');
-$routes->get('/requests/(:num)', 'Requests::GetRequestById/$1');
+$routes->post('/requests', 'Requests::List');
 $routes->get('/requests/new', 'Requests::Create');
-//$routes->post('/requests/new', 'Requests::Create');
-$routes->post('/requests/edit', 'Requests::Edit');
+$routes->post('/requests/new', 'Requests::Create');
+$routes->get('/requests/(:num)', 'Requests::GetRequestById/$1');
+$routes->post('/requests/(:num)', 'Requests::GetRequestById/$1');
+//$routes->post('/requests/edit', 'Requests::Edit');
 $routes->get('/requests/edit/(:num)', 'Requests::Edit/$1');
 
-/*
-$routes->get('/samples/(:num)', '');
-$routes->get('/samples/(:num)/details', '');
-$routes->get('/samples/new', '');
 
+$routes->get('/samples/(:num)', 'Samples::Edit/$1');
+$routes->get('/samples/(:num)/details', 'Samples::Edit/$1');
+$routes->post('/samples/new', 'Samples::Create');
+
+/*
 $routes->get('/measurements/(:num)', '');
 $routes->get('/measurements/new', '');
 */
--- app/Controllers/Requests.php
+++ app/Controllers/Requests.php
@@ -30,18 +30,8 @@ class Requests extends BaseController
 
     public function Create()
     {
-        $model = model(\App\Models\API::class);       
-        $projectList = $model->GetProjectListAsArray();
+        $model = model(\App\Models\API::class);   
 
-        $view = StdView::Begin('New request');
-        $view .= RequestView::CreateForm($projectList);
-
-        return $view;   
-    }
-
-    public function Edit($id=null)
-    {
-        $model = model(\App\Models\API::class);
         $post = $this->request->getPost();
         if($post)
         {
@@ -50,26 +40,60 @@ class Requests extends BaseController
             if(!$result->error)
             {
                 $id = $result->body->inserted;
+                return redirect()->to(base_url('requests/' . $id));
             }
             else
             {
                 //error inserting record
             }
         }
+        else
+        {
+            $projectList = $model->GetProjectListAsArray();
+            $view = StdView::Begin('New request');
+            $view .= RequestView::CreateForm($projectList);
+
+            return $view;   
+        }    
+        
+    }
+
+    public function Edit($id)
+    {
+        $model = model(\App\Models\API::class);
 
         $filter = json_encode(['RequestID' => $id]);
         $samples = $model->GetTable('Samples', $filter)->body->data;
-        $request = $model->GetRecord('Requests', $id);
-        //var_dump($samples);
+        $request = $model->GetRecord('Requests', $id)->body->data[0];
+        
         $view = StdView::Begin('Edit request #' . $id);
-        $view .= RequestView::ShowDetails($request->body->data[0]);
-        $view .= SampleView::List($samples);
+        var_dump('Edit form...');
+        //$view .= RequestView::ShowDetails($request);
+        //$view .= SampleView::NewPostButton($id);
+        //$view .= SampleView::List($id, $samples);
 
         return $view;
     }
 
     public function GetRequestById($id)
     {
-        $api = model(\App\Models\API::class);
+        $model = model(\App\Models\API::class);
+        $post = $this->request->getPost();
+        if(isset($post['del']))
+        {
+            //Sample deletion request
+            $model->deleteRecord('Samples', $post['del']);
+        }
+
+        $filter = json_encode(['RequestId' => $id]);
+        $samples = $model->GetTable('Samples', $filter)->body->data;
+        $request = $model->GetRecord('Requests', $id)->body->data[0];
+        
+        $view = StdView::Begin('Request #' . $id . ' details');
+        $view .= RequestView::ShowDetails($request);
+        $view .= SampleView::NewPostButton($id);
+        $view .= SampleView::List($id, $samples);
+        
+        return $view;
     }
 }
